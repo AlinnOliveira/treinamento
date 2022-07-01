f9ae59f28d803a55a20a44e56c904472428d7359
aline <aline_oliveira@yahoo.com>
2022-06-30 21:34:25 -0300

    etapta de estudos JavaScript. Mapas mentais de estudos,

---
+++ 04_JavaScript/teste.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+//Descobrindo os  números pares e ímpares
+function returnEvenValues(array){
+
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++){
+        if (array[i] % 2 == 0 ){
+            evenNums.push(array[i]);
+        }else{
+            console.log(`${array[i]} é ímpar. `);
+        }
+    }
+    console.log(' Números pares: ', evenNums);
+}
+
+let array = [1,3,6,7,9,11,13,14,16];
+returnEvenValues(array);
