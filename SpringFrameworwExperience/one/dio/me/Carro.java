a430909042b291655e9b56795386b9cad2f26aea
aline <aline_oliveira@yahoo.com>
2022-07-26 20:42:07 -0300

    exercicios POO

--- SpringFrameworwExperience/one/dio/me/Carro.java
+++ SpringFrameworwExperience/one/dio/me/Carro.java
@@ -8,4 +8,39 @@ public class Carro {
     String modelo;
     String cor;
 
+    Carro(){
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidade = capacidadeTanque;
+    }
+    
+    public int getCapacidade() {
+        return capacidade;
+    }
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+   
+    double totalValorTanque(double valorCombustivel){
+        return capacidade * valorCombustivel;
+    }
+
+
 }
+
+
