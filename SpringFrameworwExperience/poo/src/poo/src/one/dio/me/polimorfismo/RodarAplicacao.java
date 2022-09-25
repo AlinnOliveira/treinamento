cd3ec61361423fcf24ad10629d725b618a4e125c
Aline Oliveira <aline_oliveira@yahoo.com>
2022-09-25 06:11:52 -0300

    gitignore

--- SpringFrameworwExperience/poo/src/poo/src/one/dio/me/polimorfismo/RodarAplicacao.java
+++ SpringFrameworwExperience/poo/src/poo/src/one/dio/me/polimorfismo/RodarAplicacao.java
@@ -3,7 +3,7 @@ package poo.src.one.dio.me.polimorfismo;
 public class RodarAplicacao {
 
     public static void main(String[] args) {
-        
+        /* 
         ClasseMae[] classes = new ClasseMae[]{new ClasseFilha1(), new ClasseFilha2(), new ClasseMae()};
         for (ClasseMae classe: classes){
             classe.metodo1();
@@ -17,6 +17,25 @@ public class RodarAplicacao {
 
         ClasseFilha2 classeFilha2 = new ClasseFilha2();
         classeFilha2.metodo2();
+        
+        int i = 1234;
+        int vezes = 0;
+        while(i>=99){
+            if(i%4==0) 
+                i--;
+            i = i/3;
+            vezes++;
+        }
+        System.out.println(i);
+        System.out.println(vezes);
+        */
+        int numero = 1; 
+        int soma = 0;
+        while (numero < 5){
+            soma = soma + numero;
+            numero = numero + 1; 
+        }
+        System.out.println(soma);
     }
     
 }
